2f0c47c3c1911eaf65aa708512cb35f364694c72
DOLSHEI <dolshei@gmail.com>
2022-12-28 23:28:55 +0900

    Mybatis, mariaDB 연동

---
+++ src/main/java/com/template/springboot/controller/Home2Controller.java
@@ -0,0 +1,31 @@
+package com.template.springboot.controller;
+
+import com.template.springboot.model.SalaryModel;
+import com.template.springboot.service.SalaryService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class Home2Controller {
+
+    @Autowired
+    SalaryService salaryService;
+
+    @RequestMapping(value = "/home2", method = RequestMethod.GET)
+    public ModelAndView goHome(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+
+        List<SalaryModel> salaryList = salaryService.getSalary();
+
+        mav.addObject("salaryList", salaryList);
+        mav.setViewName("content/home.html");
+
+        return mav;
+    }
+}
