da90599d545889a556255503c097b3c527ad74eb
Dolshei <dolshei@gmail.com>
2022-12-30 07:31:51 +0900

    Refactoring

--- src/main/java/com/template/springboot/config/MariaDBConfig.java
+++ src/main/java/com/template/springboot/config/MariaDBConfig.java
@@ -5,7 +5,6 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
@@ -16,16 +15,19 @@ public class MariaDBConfig {
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        final SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource);
+        final SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactoryBean.setMapperLocations(resolver.getResources("classpath:mybatis/mapper/*.xml"));
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:mybatis/mapper/*.xml"));
+        sqlSessionFactoryBean.setConfigLocation(resolver.getResource("classpath:mybatis/mybatis-config.xml"));
 
+/*
         Resource myBatisConfig = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis/mybatis-config.xml");
-        sessionFactoryBean.setConfigLocation(myBatisConfig);
+        sqlSessionFactoryBean.setConfigLocation(myBatisConfig);
+*/
 
-        return sessionFactoryBean.getObject();
+        return sqlSessionFactoryBean.getObject();
     }
 
 }
